1c2f49c0ed5dcebe5a67fc35f36eef48b1a5dfef
yeemaster <1767870073@qq.com>
2017-09-13 14:54:55 +0800

    reformat code

--- src/app.js
+++ src/app.js
@@ -1,146 +1,130 @@
-// import React,{Component} from 'react'
-// import {Provider} from 'react-redux'
-// import configureStore from './store/configureStore.js'
-// import AppWithNavigationState from './AppWithNavigationState.js'
+/**
+ * 引导页
+ */
 
-
-// const store = configureStore();  
-
-// export default class App extends Component{
-// 	render(){
-// 		return(
-//            <Provider store={store}>
-//              <AppWithNavigationState />
-//            </Provider>
-// 		)
-// 	}
-// }
-
-
-
-import React,{Component} from 'react'
-import {StyleSheet,View,Text} from 'react-native'
+import React, {Component} from 'react'
+import {StyleSheet, View, Text} from 'react-native'
 import {Provider} from 'react-redux'
 import configureStore from './store/configureStore.js'
 import AppWithNavigationState from './AppWithNavigationState.js'
 import Splash from './component/Splash.js'
 import AppIntro from 'react-native-app-intro';
 
+const store = configureStore();
 
-const store = configureStore();  
+export default class App extends Component {
 
-export default class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSplash: true,
+            showIntro: true
+        };
+        this.hideSplashShow = this.hideSplashShow.bind(this);
+        this.onSkipBtnHandle = this.onSkipBtnHandle.bind(this);
+        this.doneBtnHandle = this.doneBtnHandle.bind(this);
+        this.onSlideChangeHandle = this.onSlideChangeHandle.bind(this);
+    }
 
-	constructor(props) {
-	  super(props);
-	  this.state = {
-	  	 showSplash : true,
-	  	 showIntro : true
-	  };
-	  this.hideSplashShow = this.hideSplashShow.bind(this);
-	  this.onSkipBtnHandle = this.onSkipBtnHandle.bind(this);
-	  this.doneBtnHandle = this.doneBtnHandle.bind(this);
-	  this.onSlideChangeHandle = this.onSlideChangeHandle.bind(this);
-      
-	}
+    hideSplashShow() {
+        this.setState({
+            showSplash: false
+        })
+    }
 
-	hideSplashShow(){
-	  this.setState({
-	  	 showSplash : false
-	  })
-	}
-	onSkipBtnHandle(index){
-	    // Alert.alert('Skip');
-	    console.log(index);
-	    this.setState({
-	  	   showIntro : false
-	    })
-	}
-	doneBtnHandle(){
-	    // Alert.alert('Done');
-	    this.setState({
-	  	   showIntro : false
-	    })
+    onSkipBtnHandle(index) {
+        this.setState({
+            showIntro: false
+        })
+    }
 
-	}
-	nextBtnHandle(index){
-	   // Alert.alert('Next');
-	   console.log(index);
-	}
-	onSlideChangeHandle(index, total){
-	   if(index == total - 1){
-		    this.setState({
-		  	   showIntro : false
-		    })	   	
-	   }
-	}
- 	
+    doneBtnHandle() {
+        this.setState({
+            showIntro: false
+        })
 
-	render(){
-		return(
-		   this.state.showSplash ? 	
-		       <Splash hideSplashShow={this.hideSplashShow} /> : 
-		       (
-			   	    this.state.showIntro ? 
-			   	    (
+    }
 
-						<AppIntro
-						  onNextBtnClick={this.nextBtnHandle}
-						  onDoneBtnClick={this.doneBtnHandle}
-						  onSkipBtnClick={this.onSkipBtnHandle}
-						  onSlideChange={this.onSlideChangeHandle}
-						  doneBtnLabel='主页面'
-						  skipBtnLabel='跳过'
-						  showSkipButton={false}
-						  showDoneButton={false}
-						  showDots={false}
-						>
-						<View style={[styles.slide,{ backgroundColor: '#999' }]}>
-						  <View level={10} style={styles.circle}><Text style={styles.text}>1</Text></View>
-						</View>
-						<View style={[styles.slide, { backgroundColor: '#eee' }]}>
-						  <View level={10} style={styles.circle}><Text style={{fontSize:30,fontWeight:'bold',color:'#000'}}>2</Text></View>
-						</View>
-						<View style={[styles.slide,{ backgroundColor: '#999' }]}>
-						  <View level={10} style={styles.circle}><Text style={styles.text}>3</Text></View>
-						</View>
-						<View style={[styles.slide, { backgroundColor: '#eee' }]}>
-						  <View level={10} style={styles.circle}><Text style={{fontSize:30,fontWeight:'bold',color:'#000'}}>开始</Text></View>
-						</View>
-						</AppIntro>
-			   	    ) : 
-			   	    (
-		               <Provider store={store}>
-		                  <AppWithNavigationState />
-		               </Provider> 
-	                )
+    nextBtnHandle(index) {
+        console.log(index);
+    }
+
+    onSlideChangeHandle(index, total) {
+        if (index == total - 1) {
+            this.setState({
+                showIntro: false
+            })
+        }
+    }
+
+
+    render() {
+        return (
+            this.state.showSplash ?
+                <Splash hideSplashShow={this.hideSplashShow}/> :
+                (
+                    this.state.showIntro ?
+                        (
+
+                            <AppIntro
+                                onNextBtnClick={this.nextBtnHandle}
+                                onDoneBtnClick={this.doneBtnHandle}
+                                onSkipBtnClick={this.onSkipBtnHandle}
+                                onSlideChange={this.onSlideChangeHandle}
+                                doneBtnLabel='主页面'
+                                skipBtnLabel='跳过'
+                                showSkipButton={false}
+                                showDoneButton={false}
+                                showDots={false}
+                            >
+                                <View style={[styles.slide,{ backgroundColor: '#999' }]}>
+                                    <View level={10} style={styles.circle}><Text style={styles.text}>1</Text></View>
+                                </View>
+                                <View style={[styles.slide, { backgroundColor: '#eee' }]}>
+                                    <View level={10} style={styles.circle}><Text
+                                        style={{fontSize:30,fontWeight:'bold',color:'#000'}}>2</Text></View>
+                                </View>
+                                <View style={[styles.slide,{ backgroundColor: '#999' }]}>
+                                    <View level={10} style={styles.circle}><Text style={styles.text}>3</Text></View>
+                                </View>
+                                <View style={[styles.slide, { backgroundColor: '#eee' }]}>
+                                    <View level={10} style={styles.circle}><Text
+                                        style={{fontSize:30,fontWeight:'bold',color:'#000'}}>开始</Text></View>
+                                </View>
+                            </AppIntro>
+                        ) :
+                        (
+                            <Provider store={store}>
+                                <AppWithNavigationState />
+                            </Provider>
+                        )
                 )
-		)
-	}
+        )
+    }
 }
 
 
 const styles = StyleSheet.create({
-  slide: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-    padding: 15,
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
-  circle : {
-  	 width : 80,
-  	 height : 80,
-  	 borderRadius : 40,
-  	 borderWidth : 5,
-  	 borderColor :'#000',
-  	 borderStyle : 'solid',
-  	 justifyContent : 'center',
-  	 alignItems : 'center'
-  }
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB',
+        padding: 15,
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    circle: {
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        borderWidth: 5,
+        borderColor: '#000',
+        borderStyle: 'solid',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
